1ed1c9de2f12e9d3876ddfa448bc15123fcff9e4
Basil <basiltalias92@gmail.com>
2017-01-18 00:58:36 -0500

    - Added buttons - Added number references - Added operators symbol reference

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numberScreen:Double = 0;
+    @IBOutlet weak var label: UILabel!
+    @IBAction func numbers(_ sender: UIButton) {
+        label.text = label.text! + String(sender.tag-1)
+        numberScreen = Double(label.text!)!
+    }
+    
+    @IBAction func operatorBtn(_ sender: UIButton) {
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
